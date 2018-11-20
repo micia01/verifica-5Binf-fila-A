10e4c724a07663797e3aca16c9440a9af16a14f2
SebastianTura <>
2018-11-20 11:22:38 +0100

    consegna verifica YOUNES 5Binf

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,27 +1,100 @@
 
-// TODO Inserire il package corretto
+// DONE Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica;
 
 import javax.swing.*;
 
 public class Main extends JFrame {
 	
 	public Main() {
-		super("Formula 1 Passion");
+		super("Color Picker");
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
-		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		// DONE Inserire la size in accordo con il wireframe (valori approssimati)
+		super.setSize(300,900);
 		
-		// TODO Usare StringBuilder per costruire la stringa HTML
+		// DONE Usare StringBuilder per costruire la stringa HTML
+		StringBuilder stbuild = new StringBuilder();
 		
-		// TODO Creare la stringa HTML per il wireframe
+		// DONE Creare la stringa HTML per il wireframe
+		stbuild.append("<html>");
+		stbuild.append("<head>");
+
+		stbuild.append("<h1>Color Picker</h1>");
 		
-		// TODO Convertire lo StringBuilder in String
+		stbuild.append("</head>");
+		stbuild.append("<body>");
+		stbuild.append("<div style='display:table;width:100%'>");//OPEN TABLE
+		//FIRST ROW
+		stbuild.append("<div style='display:table-row;'>");
 		
-		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("<b>Name</b>");
+		stbuild.append("</div>");
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("<b>Hex</b>");
+		stbuild.append("</div>");
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("<b>RGB</b>");
+		stbuild.append("</div>");
+		
+		stbuild.append("</div>");
+			
+		//SECOND ROW
+		stbuild.append("<div style='display:table-row;'>");
+			
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("Persian Green");
+		stbuild.append("</div>");
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("#00A693");
+		stbuild.append("</div>");
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("0,166,147");
+		stbuild.append("</div>");
+			
+		stbuild.append("</div>");
+			
+		//THIRD ROW
+		stbuild.append("<div style='display:table-row;'>");
+			
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("Venice Blue");
+		stbuild.append("</div>");
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("#055989");
+		stbuild.append("</div>");
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("5,89,137");
+		stbuild.append("</div>");
+			
+		stbuild.append("</div>");
+		
+		//FOURTH ROW
+		stbuild.append("<div style='display:table-row;'>");
+			
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("Pale Blue");
+		stbuild.append("</div>");
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("#73D1E4");
+		stbuild.append("</div>");
+		stbuild.append("<div style='display:table-cell;'>");
+		stbuild.append("115,209,228");
+		stbuild.append("</div>");
+			
+		stbuild.append("</div>");
+		
+		stbuild.append("</div>"); //END TABLE
+		stbuild.append("</body>");
+		stbuild.append("</html>");
+		
+		// DONE Convertire lo StringBuilder in String
+		String strHtml = stbuild.toString();
+		
+		// DONE Assegnare correttamente la stringa create a JLabel
+		super.add(new JLabel(strHtml));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
